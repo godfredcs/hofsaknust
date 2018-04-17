ebbb2bd58bcf04cbccca9321c0ae9fcc59d37de4
godfredcs <addaiz.godfred@gmail.com>
2018-04-17 16:36:37 +0000

    Working on refactoring the template

---
+++ src/components/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="wrapper">
+	  <div id="upfront">
+	  	<app-header />
+		
+		<div class="front">
+			<div>
+				<h1 class="text-white">Welcome to hofsa knust</h1>
+				<p style="color: #AAA;">We are still in the period of 40 days fasting and prayers and you are humbly welcome to join</p>
+			</div>
+		</div>
+	  </div>
+
+	  <app-footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/common/Header.vue';
+import Footer from '@/components/common/Footer.vue';
+
+export default {
+	components: {
+		appHeader: Header,
+		appFooter: Footer,
+	}
+}
+</script>
+
+<style scoped>
+#wrapper {
+	
+}
+
+#upfront {
+	height: 100vh;
+	background-color: #000;
+	margin: auto auto;
+}
+
+.front {
+	padding-top: 100px;
+	height: inherit;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+.text-white {
+	color: #FFF;
+}
+</style>
---
+++ src/components/common/Header.vue
@@ -0,0 +1,47 @@
+<template>
+	<header id="header">
+		<div class="container-fluid">
+
+			<div id="logo" class="pull-left">
+				<h1><a href="#intro" class="scrollto">Hofsa</a></h1>
+				<!-- Uncomment below if you prefer to use an image logo -->
+				<!-- <a href="#intro"><img src="img/logo.png" alt="" title="" /></a>-->
+			</div>
+
+			<nav id="nav-menu-container">
+				<ul class="nav-menu">
+					<li class="menu-active"><a href="#intro">Home</a></li>
+					<li><a href="#campus-connect">Campus Connect</a></li>
+					<li><a href="#hofsa-organogram">Organogram</a></li>
+					<li><a href="#missions">Missions</a></li>
+					<li><a href="#projects">Projects & Programmes</a></li>
+					<li><a href="#projects">Support Now</a></li>
+					<li><a href="#projects">Partnerships</a></li>
+					<li><a href="#projects">Gallery</a></li>
+					<li><a href="#projects">Calendar</a></li>
+					<!-- li class="menu-has-children"><a href="">Drop Down</a>
+						<ul>
+						<li><a href="#">Drop Down 1</a></li>
+						<li><a href="#">Drop Down 3</a></li>
+						<li><a href="#">Drop Down 4</a></li>
+						<li><a href="#">Drop Down 5</a></li>
+						</ul>
+					</li -->
+				</ul>
+			</nav><!-- #nav-menu-container -->
+		</div>
+  </header><!-- #header -->
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+	header {
+		background-color: #000;
+	}
+</style>
+
